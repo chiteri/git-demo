a52587493c876f88db41cc971639dab2d031dcbb
Martin Chiteri <martin.chiteri@gmail.com>
2022-12-26 16:38:51 -0700

    Add Math functions module to repository

---
+++ my_math.py
@@ -0,0 +1,16 @@
+# Addition operation 
+def add(a, b):
+    return a + b
+
+# Subtraction operation 
+def subtract(a, b):
+    return a - b 
+
+# Multiplication operation
+def multiply(a, b):
+    return a * b 
+
+# Division operation
+def divide(a, b):
+    return a // b  
+
